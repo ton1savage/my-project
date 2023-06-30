4806634bc3d7021011ce3b17426341e3c5b06df3
ton1savage <81397918+ton1savage@users.noreply.github.com>
2023-06-29 18:06:57 -0700

    stripe import error

---
+++ src/api/order/controllers/order.js
@@ -0,0 +1,11 @@
+
+'use strict';
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+/**
+ * order controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::order.order');
